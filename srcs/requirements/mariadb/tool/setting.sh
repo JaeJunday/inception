f511efb27088eb9c66074bc4d5dc8c396e1ac11e
jaejkim <jaejkim@jaejkim.jaejkim.42.fr>
2023-06-26 22:15:26 +0900

    Feat mariadb setting script

---
+++ srcs/requirements/mariadb/tool/setting.sh
@@ -0,0 +1,5 @@
+mysql -u root -e "set password for 'root'@'localhost' = password('$MYSQL_ROOT_PASSWORD');"
+mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
+mysql -u root -e "CREATE user $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME identified by '$WORDPRESS_DB_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME;"
+mysql -u root -e "FLUSH PRIVILEGES;"
