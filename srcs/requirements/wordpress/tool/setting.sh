f23ab07d8254969c2487fd1ac56e27043d1140ff
jaejkim <jaejkim@jaejkim.jaejkim.42.fr>
2023-06-26 15:06:52 +0900

    <Rename>  dockerignore move root

--- srcs/requirements/wordpress/tool/setting.sh
+++ srcs/requirements/wordpress/tool/setting.sh
@@ -1,6 +1,10 @@
+#!bin/bash
 
+# unzip wordpress
 tar -xzf /tmp/wordpress.tar.gz -C /var/www/html/ 
 
+# nginx authority allowed
 chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
 
+# run php-fpm7.4
 ./usr/sbin/php-fpm7.4 -F
