c82e2612324df0d079aac7b714c04f70875a8628
jaejkim <jaejkim@jaejkim.jaejkim.42.fr>
2023-06-28 21:33:59 +0900

    hotfix: wordpress wp-config.php not cp in container modified

--- srcs/requirements/mariadb/tools/setting.sh
+++ srcs/requirements/mariadb/tools/setting.sh
@@ -4,13 +4,6 @@ if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
 
     service mariadb start
 
-    echo $WORDPRESS_DB_PASSWORD
-    echo $WORDPRESS_DB_USER
-    echo $WORDPRESS_DB_NAME
-    echo $
-
-
-    mysql -u root -e "set password for 'root'@'localhost' = password('$MYSQL_ROOT_PASSWORD');"
     mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
     mysql -u root -e "CREATE user $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME identified by '$WORDPRESS_DB_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME;"
--- srcs/requirements/wordpress/tool/setting.sh
+++ srcs/requirements/wordpress/tool/setting.sh
@@ -3,6 +3,8 @@
 # unzip wordpress
 tar -xzf /tmp/wordpress.tar.gz -C /var/www/html/ 
 
+cp /tmp/wp-config.php /var/www/html/wordpress/wp-config.php
+
 # nginx authority allowed
 chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
 
