205955207e738717a65320d5e520c3c914e2618f
jaejkim <jaejkim@jaejkim.jaejkim.42.fr>
2023-06-28 22:07:20 +0900

    fix: db connection complete

--- srcs/requirements/mariadb/tools/setting.sh
+++ srcs/requirements/mariadb/tools/setting.sh
@@ -1,15 +1,16 @@
+#! /bin/bash
 if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
-    
+    echo "=> Setting MariaDB Create Database..."
     mariadb-install-db
-
     service mariadb start
-
     mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
-    mysql -u root -e "CREATE user $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME identified by '$WORDPRESS_DB_PASSWORD';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$WORDPRESS_DB_NAME;"
+    mysql -u root -e "CREATE user $WORDPRESS_DB_USER@'%' identified by '$WORDPRESS_DB_PASSWORD';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@'%';"
     mysql -u root -e "FLUSH PRIVILEGES;"
-
-   service mariadb stop
+    service mariadb stop
+    echo "=> Setting MariaDB Create Database... Done!"
+else
+    echo "=> Setting MariaDB Create Database... Already exist!"
 fi
 
 mysqld --user=root
